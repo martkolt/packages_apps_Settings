6ca3ad595769a7d89f2d42b91c9fb98f70fe86ea
maxwen <max.weninger@gmail.com>
2017-08-31 16:44:29 +0200

    Settings: Enable AAPT2 again and add OmniGears to proguard
    Change-Id: I8d722956c6a4209cf084151042db59d1cb6cac38

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
-#LOCAL_USE_AAPT2 := true
+LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../OmniGears/src)
